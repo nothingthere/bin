af5914517c3acd0237bac2e8493b931b3d929044
nothingthere <m15982038632@gmail.com>
2017-02-19 09:57:44 +0800

    初始化

---
+++ reinstall_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#apt install fcitx-table-wbpy fcitx-googlepinyin
+#sudo apt install libopencc2 fcitcx-libs fcitx-libs-qt
+sudo apt install -y nodejs
+
+sudo apt install golang
+
+sudo apt install tlp tlp-rdw
+# sudo sensors-detect
+
+sudo apt install lm-sensors chromium
+# sudo tlp start
+
+# sudo apt install chromium
+
+# sudo npm i gulp-cli -g
+# sudo dpkg -i ***lanten.deb
+sudo apt install libappindicator3-1
+
+sudo apt install ruby-full rubygems
+sudo gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
+sudo su -c "gem install sass"
